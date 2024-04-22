8e18ffb940cd7c73d95084d046bedbd0ff894a89
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2024-04-22 18:26:24 +0530

    feat: add material palette

---
+++ lib/utils/theme/convertPropertyToCSSVariableName.ts
@@ -0,0 +1,30 @@
+/**
+ * @license
+ * Material You React Package
+ * Copyright (C) 2024  Rutaj Dash
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, only version 3 of the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The contact information of the author and copyright owner of this
+ * program can be found at <https://github.com/rutajdash>
+ */
+
+/**
+ * Converts a property name to a hyphenated CSS variable name.
+ *
+ * @param name The property name to convert.
+ *
+ * @returns The converted CSS variable name.
+ */
+export default function convertPropertyToCSSVariableName(name: string): string {
+  return name.replace(/([A-Z])/g, '-$1').toLowerCase();
+}
